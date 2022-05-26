35a53f09d365abf4004e4397fcbcc63bbdc5ce1b
wuxjxj <angelxiaojiwu@gmail.com>
2022-05-26 00:15:13 -0400

    js

---
+++ script.js
@@ -0,0 +1,11 @@
+const gallery=document.getElementById('gallery');
+const popup=document.getElementById("popup");
+const selectedImage=document.getElementById('selectedImage');
+const imageIndexes=[1,2,3,4,5,6,7,8,9,10];
+const selectedIndex=null;
+
+imageIndexes.forEach(i =>{
+  const image=document.createElement('img');
+  image.src=`/images/sample-${i}.jpg`;
+  gallery.appendChild(image);
+})
